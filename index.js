74747a25a738d2519264981118d0eb3a1ef5c529
Naomi Dennis <njdennis94@gmail.com>
2018-07-27 15:38:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function driverNamesWithRevenueOver(list, revenue){
   driversWithRevenueOver(list, revenue).map( (ele) => { return ele['name']} )
 }
 function exactMatch(list, attribute){
-  list.filter( (ele) => { return ele ? ele['name'] === attribute || ele['revenue'] == attribute })
+  list.filter( (ele) => { return ele ? ele['name'] === attribute : ele['revenue'] == attribute })
 }
 
 function exactMatchToList(){
